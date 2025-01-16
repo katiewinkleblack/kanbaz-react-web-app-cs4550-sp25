ae85580dfc25a33ce6d25cba68da883c4ffff643
Katie Winkleblack <kmwink21@gmail.com>
2025-01-16 16:35:50 -0500

    Added Calender and Inbox

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -20,6 +20,8 @@ export default function Kambaz() {
             <Route path="/Account/*" element={<Account />} />
             <Route path="/Dashboard" element={<Dashboard />} />
             <Route path="/Courses/:cid/*" element={<Courses/>} />
+            <Route path="/Calender" element={<h2>Calender</h2>} />
+            <Route path="/Inbox" element={<h2>Inbox</h2>} />
             </Routes>
             </tr>
         </table>
