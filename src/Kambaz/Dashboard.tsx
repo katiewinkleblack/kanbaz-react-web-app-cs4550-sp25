02ac56564a276cd3a9172bad95499a5ba1d02d18
Katie Winkleblack <katiewinkleblack@Katies-MacBook-Pro.local>
2025-03-25 22:50:59 -0400

    Trying LocalHost changes

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -28,9 +28,9 @@ const [editingCourse, setEditingCourse] = useState<any | null>(null);
 
 
 const isStudent = currentUser?.role === "STUDENT";
-const [showAllCourse, setShowAllCourse] = useState(false);
+//const [showAllCourse, setShowAllCourse] = useState(false);
 
-const changeCourseView = () => setShowAllCourse(!showAllCourse);
+//const changeCourseView = () => setShowAllCourse(!showAllCourse);
 
 const handleEnroll = async (courseId: string) => {
   try {
@@ -56,21 +56,17 @@ const handleUnenroll = async (courseId: string) => {
 
 const fetchCourses = async () => {
   try {
-    if (!showAllCourse) {
       console.log("courses");
       const usercourses = await UserClient.findMyCourses();
       setCourses(usercourses);
-    } else {
-    const courses = await CoursesClient.fetchAllCourses();
-    setCourses(courses);
-    }
-  } catch (error) {
+    } 
+     catch (error) {
     console.error(error);
   }
 };
 useEffect(() => {
   fetchCourses();
-}, [currentUser, showAllCourse, enrollments]);
+}, [currentUser, enrollments]);
 
 
 
@@ -121,13 +117,7 @@ const handleUpdateCourse = () => {
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
 
-      {isStudent && (
-        <Button variant="primary" onClick={changeCourseView}
-        id="wd-show-course-button"
-         className="mb-3">
-          {showAllCourse ? "Show Enrolled Courses" : "Show All Courses"}
-        </Button>
-      )}
+     
 
     <h2 id="wd-dashboard-published">
         {isStudent ? "Courses" : `Published Courses (${courses.length})`}
