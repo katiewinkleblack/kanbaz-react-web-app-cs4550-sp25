606a7ee5a0f30667cab76f8706121af873f5fbd2
Katie Winkleblack <katiewinkleblack@Katies-MacBook-Pro.local>
2025-03-26 09:27:35 -0400

    redeploy

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -57,7 +57,7 @@ const handleUnenroll = async (courseId: string) => {
 const fetchCourses = async () => {
   try {
     if (!showAllCourse) {
-      console.log("courses")
+      
       const usercourses = await UserClient.findMyCourses();
       setCourses(usercourses);
     } else {
