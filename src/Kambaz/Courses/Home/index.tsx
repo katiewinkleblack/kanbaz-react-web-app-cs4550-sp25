dc9ab94979c96430d7375f99fbb58c66da878a99
Katie Winkleblack <katiewinkleblack@Katies-MacBook-Pro.local>
2025-03-31 09:22:51 -0400

    test

--- src/Kambaz/Courses/Home/index.tsx
+++ src/Kambaz/Courses/Home/index.tsx
@@ -1,6 +1,7 @@
 import Modules from "../Modules/Modules";
 import CourseStatus from "./Status";
 export default function Home() {
+  console.log("test")
   return (
     <div className="d-flex" id="wd-home">
         <div className="flex-fill me-5">
@@ -8,6 +9,7 @@ export default function Home() {
           </div>
           <div className="d-none d-xl-block">
           <CourseStatus />
+          
         </div>
         </div>
 );}
